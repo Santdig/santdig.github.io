03ca90a70dfbf34455a74c9c9f9ce3aa4158caa5
Santosh Dighe <sdighe1405@gmail.com>
2020-08-11 14:13:49 +0530

    Initial code

---
+++ js/custom-alias_1.js
@@ -0,0 +1,168 @@
+(function() {
+	'use strict';
+
+	/*----------------------------------------
+		Detect Mobile
+	----------------------------------------*/
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
+
+	// navigation
+	var OnePageNav = function() {
+		var navToggler = $('.navbar-toggler');
+		$(".smoothscroll[href^='#'], #pb-navbar ul li a[href^='#']").on('click', function(e) {
+		 	e.preventDefault();
+		 	var hash = this.hash;
+
+		 	$('html, body').animate({
+
+		    scrollTop: $(hash).offset().top
+		  }, 700, 'easeInOutExpo', function(){
+		    window.location.hash = hash;
+		  });
+		});
+		$("#pb-navbar ul li a[href^='#']").on('click', function(e){
+			if ( navToggler.is(':visible') ) {
+		  	navToggler.click();
+		  }
+		});
+
+		$('body').on('activate.bs.scrollspy', function () {
+		  console.log('nice');
+		})
+	};
+
+
+	var smoothScroll = function() {
+		var $root = $('html, body');
+
+		$('.smoothscroll').click(function () {
+			$root.animate({
+		    scrollTop: $( $.attr(this, 'href') ).offset().top
+			}, 500);
+			return false;
+		});
+	};
+
+	var magnificPopupControl = function() {
+
+
+		$('.image-popup').magnificPopup({
+			type: 'image',
+			removalDelay: 300,
+			mainClass: 'mfp-with-zoom',
+			gallery:{
+				enabled:true
+			},
+			zoom: {
+				enabled: true, // By default it's false, so don't forget to enable it
+
+				duration: 300, // duration of the effect, in milliseconds
+				easing: 'ease-in-out', // CSS transition easing function
+
+				// The "opener" function should return the element from which popup will be zoomed in
+				// and to which popup will be scaled down
+				// By defailt it looks for an image tag:
+				opener: function(openerElement) {
+				// openerElement is the element on which popup was initialized, in this case its <a> tag
+				// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+				}
+			}
+		});
+
+		$('.with-caption').magnificPopup({
+			type: 'image',
+			closeOnContentClick: true,
+			closeBtnInside: false,
+			mainClass: 'mfp-with-zoom mfp-img-mobile',
+			image: {
+				verticalFit: true,
+				titleSrc: function(item) {
+					return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+				}
+			},
+			zoom: {
+				enabled: true
+			}
+		});
+
+
+		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
+    });
+	};
+
+
+
+
+	var portfolioMasonry = function() {
+ $('.filters ul li').click(function(){
+        $('.filters ul li').removeClass('active');
+        $(this).addClass('active');
+
+        var data = $(this).attr('data-filter');
+        $grid.isotope({
+          filter: data
+        })
+      });
+
+
+      if(document.getElementById("section-portfolio")){
+            var $grid = $(".grid").isotope({
+              itemSelector: ".all",
+              percentPosition: true,
+              masonry: {
+                columnWidth: ".all"
+              }
+            })
+      };
+
+
+	};
+
+
+	$(function(){
+
+		OnePageNav();
+		offCanvasNav();
+
+		contentWayPoint();
+		navbarState();
+		// stellarInit();
+		clickMenu();
+		// navigationSection();
+		// magnificPopupControl();
+		smoothScroll();
+		portfolioMasonry();
+	});
+
+
+
+
+})();
