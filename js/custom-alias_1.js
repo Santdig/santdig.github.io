24695bb842fe49081041f9c214ceabf9d09fe398
Santosh Dighe <sdighe1405@gmail.com>
2020-08-12 21:01:47 +0530

    Update custom-alias_1.js

--- js/custom-alias_1.js
+++ js/custom-alias_1.js
@@ -217,6 +217,45 @@
 		  });
 	};
 
+	/*----------------------------------------
+		Animate Scroll
+	----------------------------------------*/
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.site-animate').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('site-animated') ) {
+
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .site-animate.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn site-animated');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft site-animated');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight site-animated');
+							} else {
+								el.addClass('fadeInUp site-animated');
+							}
+							el.removeClass('item-animate');
+						},  k * 100, 'easeInOutExpo' );
+					});
+
+				}, 100);
+
+			}
+
+		} , { offset: '95%' } );
+	};
+
 
 	$(function(){
 
